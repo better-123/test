acfff50951c1a5bdbb9d28d506e46ff5c0bdd3ca
better <wz547114264@qq.com>
2020-02-18 11:25:07 +0800

    修复了1.0版本bug的1.1版本

--- testBRANCH/testBRANCH/ViewController.swift
+++ testBRANCH/testBRANCH/ViewController.swift
@@ -15,6 +15,11 @@ class ViewController: UIViewController {
         
         print("1.0开发版本完成")
         
+        
+    }
+    func fixbug() {
+        
+        print("修复了1.0版本的bug的1.1版本")
     }
 
 
